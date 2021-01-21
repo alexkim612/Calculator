fb74685ca3dafa88101ad6a7dd30eeb1d5564404
Alex Kim <alexkim612@gmail.com>
2021-01-21 01:49:52 -0600

    start history

--- client/src/components/History.jsx
+++ client/src/components/History.jsx
@@ -4,8 +4,16 @@ class History extends React.Component {
   
   render() {
     return (
-      <div>
-        History
+      <div className='history-wrapper'>
+        <h3 className='history-header'>History</h3>
+
+        <div className='history-feed'>
+          <p>ID : 1+1=2</p>
+          <p>ID : 2+2=4</p>
+          <p>ID : 2+2=4</p>
+          <p>ID : 2+2=4</p>
+          <p>ID : 2+2=4</p>
+        </div>
       </div>
     )
   }
